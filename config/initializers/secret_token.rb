7d427d1161788dd27aae632560ad7b1c2f606752
JianingZheng <jnzheng@thoughtworks.com>
2012-09-13 22:36:14 +0800

    create a new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoogleMapApp::Application.config.secret_token = '172c71463a57f2d256c72bc84ffd428685b2ac3add69abfcab838987df1acb5ae8e003098a9fd0720c56344df814634498129377138328db8d23838a69d19530'
